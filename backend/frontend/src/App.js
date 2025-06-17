03e961879817a44549daf70ba9134d02e5e01d32
Smith <mrsmithc49@gmail.com>
2025-06-17 13:21:51 +0530

    user changes

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
    <Router>
          
-                      <nav>
+            <nav>
              <div className="nav-logo">
               <Link to="/signup">Air Table</Link></div>
              <ul>
